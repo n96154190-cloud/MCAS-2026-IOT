aff853b8529a2cdb6c26690b6deddf1497865753
n96154190 <n96154190@gs.ncku.edu.tw>
2026-09-21 16:58:09 +0800

    Update Lab2

--- Lab2/app.py
+++ Lab2/app.py
@@ -1,32 +1,46 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 images = [
-    "images/pic1.jpg",
-    "images/pic2.jpg",
-    "images/pic3.jpg"
+    "images/螢幕擷取畫面 2026-09-09 165601.png",
+    "images/螢幕擷取畫面 2026-09-10 073117.png",
+    "images/螢幕擷取畫面 2026-09-10 073855.png"
 ]
 
-current_index = 0  # 全域索引
+current_index = 0
+
 
 @app.route("/")
 def index():
-    global current_index
-    img = images[current_index]
-    return render_template("index.html", image=img, idx=current_index, total=len(images))
+    return render_template(
+        "index.html",
+        title="數學公式圖庫",
+        image=images[current_index],
+        idx=current_index,
+        total=len(images)
+    )
+
 
 @app.route("/next")
 def next_img():
     global current_index
-    current_index += 1
+
+    # 到最後一張後，回到第一張
+    current_index = (current_index + 1) % len(images)
+
     return redirect(url_for("index"))
+
 
 @app.route("/prev")
 def prev_img():
     global current_index
-    current_index -= 1
+
+    # 在第一張按上一張時，跳到最後一張
+    current_index = (current_index - 1) % len(images)
+
     return redirect(url_for("index"))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050, debug=True)
